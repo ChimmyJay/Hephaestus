58bb9422fb966a2bdc7d508091ed0afe8570a967
chimmyJay <chimmyJay@outlook.com>
2021-01-25 15:54:42 +0800

    Rename FocusToolController => FocusTaskTimerController

--- Hephaestus/HephaestusWeb/Controllers/FocusTaskTimerController.cs
+++ Hephaestus/HephaestusWeb/Controllers/FocusTaskTimerController.cs
@@ -6,7 +6,7 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace HephaestusWeb.Controllers
 {
-    public class FocusToolController : Controller
+    public class FocusTaskTimerController : Controller
     {
         public IActionResult Index()
         {
